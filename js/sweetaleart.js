f2d905f59a9e9474e94eff935f7e83e1bf058dd5
SimonSulin <90248863+SimonSulin@users.noreply.github.com>
2022-01-03 11:01:03 +0100

    Delete js directory

--- js/sweetaleart.js
+++
@@ -1,9 +0,0 @@
-function sweet_aleart(){
-    Swal.fire({
-        toast: false,
-        icon: 'info',
-        title: 'This was made for a school project',
-        showConfirmButton: true,
-        position: 'center',
-    });
-}
